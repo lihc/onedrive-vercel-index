75eee82f1b92ac888a152bb0788ca13d26b385d0
李海诚 <lihaicheng.email@gmail.com>
2024-04-22 15:17:05 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/.lihc', 'config'],
+  protectedRoutes: ['/.lihc', '/config'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:lihaicheng.email@gmail.com',
